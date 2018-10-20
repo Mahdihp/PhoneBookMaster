db724ea4604ec29fe5c50a59cdc075c720be7b24
mahdi <mahdihp.devsc@gmail.com>
2018-10-20 12:27:47 +0330

    Create Rest Controller

--- src/main/java/com/mahdi/controller/UserController.java
+++ src/main/java/com/mahdi/controller/UserController.java
@@ -2,20 +2,15 @@ package com.mahdi.controller;
 
 
 import com.mahdi.common.Constants;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
-@Controller
+@RestController
 @RequestMapping(path = "/"+ Constants.KEY_USER_CONTROLLER)
 public class UserController {
 
 
 
-    @ResponseBody
     @RequestMapping(path = "/home",method = RequestMethod.GET)
     public String home(){
         return "Hello World";
